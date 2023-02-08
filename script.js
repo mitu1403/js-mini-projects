7e21b99d4cc0c8b52689554a3c0dfb80a51abf08
mitu1403 <mmitu163069@bscse.uiu.ac.bd>
2023-02-08 14:54:35 +0600

    elements created dynamically with data.json

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-const panels = document.querySelectorAll('.panel')
-panels.forEach((panel) => {
-    panel.addEventListener('click', ()=>{
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
+const container = document.querySelector(".container")
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((json) => json.quotes.forEach(quote =>{
+    // console.log(quote)
+    // create a new div with a panel class
+    const panel = document.createElement('div')
+    panel.classList.add("panel")
+    panel.innerHTML = `<h3>${quote.title}</h3>`
+    container.appendChild(panel)   
+    // 
 
-})
-function removeActiveClasses(){
-    panels.forEach((panel) => {
-        panel.classList.remove('active')        
+    // after creating, keep the elements in panels variable.
+    var panels = document.querySelectorAll(".panel");
+    panels[0].classList.add('active')    
+    //
+
+    // add eventlistener to each and every panels
+    panels.forEach((panel) =>{
+      panel.addEventListener("click", () => {
+        removeActiveClasses()
+        panel.classList.add("active")
+      })
     })
-}
+    //
+
+    // remove active class from the panels.
+    function removeActiveClasses() {
+      panels.forEach((panel) => {
+        panel.classList.remove("active");
+      });
+    }
+    //
+
+  }));
